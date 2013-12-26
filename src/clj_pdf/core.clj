4101526aa6850fd8b4c9b78e5f6ed69af5fbf8ed
Yogthos <dmitri.sotnikov@gmail.com>
2013-12-26 14:56:15 -0500

    added caching for svg elements

--- src/clj_pdf/core.clj
+++ src/clj_pdf/core.clj
@@ -567,6 +567,14 @@
         (swap! *cache* assoc chart-hash compiled)
         compiled))))
 
+(defn- svg-element [& params]
+  (let [svg-hash (java.util.Objects/hashCode params)]
+    (if-let [cached (get *cache* svg-hash)]
+      cached
+      (let [compiled (apply svg/render params)]
+        (swap! *cache* assoc svg-hash compiled)
+        compiled))))
+
 (defn- line [{dotted? :dotted, gap :gap} & args]
   (doto (if dotted?
           (if gap
@@ -617,7 +625,7 @@
             :heading     heading
             :image       image
             :graphics    g2d/with-graphics
-            :svg         svg/render
+            :svg         svg-element
             :line        line
             :list        li
             :paragraph   paragraph
